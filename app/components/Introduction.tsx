409b9c8697335298d16e9b1c8244eb473f596f07
Navishkar Rao <sirmews@gmail.com>
2023-04-07 11:22:32 +0000

    add simple intro

---
+++ app/components/Introduction.tsx
@@ -0,0 +1,15 @@
+export const Introduction = () => (
+  <div className='bg-white py-24 sm:py-32'>
+    <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+      <div className='mx-auto max-w-2xl lg:mx-0'>
+        <h2 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+          Home of Nav
+        </h2>
+        <p className='mt-6 text-lg leading-8 text-gray-600'>
+          A perfectly cromulent product engineer, aspiring indiehacker, chronic
+          oversharer of all things mental health
+        </p>
+      </div>
+    </div>
+  </div>
+)
