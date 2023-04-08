f0b798a7f190deb6c36da07c4a96b23c806b00dd
Navishkar Rao <sirmews@gmail.com>
2023-04-08 03:36:57 +0000

    homepage links

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,21 +1,26 @@
-import type { FunctionComponent, ReactElement } from "react";
-import classNames from "classnames";
+import type { FunctionComponent, ReactElement } from 'react'
+import classNames from 'classnames'
 
 interface ButtonProps {
-  children: string;
-  onClick?: () => void;
-  variant?: "primary" | "secondary";
-  size?: "xs" | "sm" | "md" | "lg" | "xl";
-  icon?: ReactElement;
+  children: string
+  onClick?: () => void
+  variant?: 'primary' | 'secondary'
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+  icon?: ReactElement
+  href?: string
 }
 
 export const Button: FunctionComponent<ButtonProps> = ({
   children,
   onClick,
-  variant = "primary",
-  size = "md",
+  variant = 'primary',
+  size = 'md',
   icon,
+  href,
 }) => {
+  const isLink = typeof href !== 'undefined'
+  const Component = isLink ? 'a' : 'button'
+
   const className = classNames(
     'rounded-md',
     'shadow-sm',
@@ -24,8 +29,10 @@ export const Button: FunctionComponent<ButtonProps> = ({
     'focus:ring-offset-2',
     'font-semibold',
     {
-      'bg-indigo-600 hover:bg-indigo-500 focus:ring-indigo-600 text-white': variant === 'primary',
-      'bg-white hover:bg-gray-50 focus:ring-gray-300 text-gray-900': variant === 'secondary',
+      'bg-slate-900 hover:bg-slate-700 focus:ring-slate-900 text-white':
+        variant === 'primary',
+      'bg-white hover:bg-gray-50 focus:ring-gray-300 text-gray-900 ring-1 ring-inset ring-gray-300':
+        variant === 'secondary',
       'text-xs': size === 'xs',
       'text-sm': size === 'sm' || size === 'md',
       'text-base': size === 'lg',
@@ -36,15 +43,20 @@ export const Button: FunctionComponent<ButtonProps> = ({
       'py-1': size === 'xs' || size === 'sm',
       'py-2': size === 'md' || size === 'lg',
       'py-3': size === 'xl',
-    },
-  );
+    }
+  )
 
   return (
-    <button type="button" onClick={onClick} className={className}>
+    <Component
+      type={isLink ? undefined : 'button'}
+      href={href}
+      onClick={onClick}
+      className={className}
+    >
       {icon && (
-        <span className="inline-flex items-center gap-x-1.5">{icon}</span>
+        <span className='inline-flex items-center gap-x-1.5'>{icon}</span>
       )}
       <span>{children}</span>
-    </button>
-  );
-};
+    </Component>
+  )
+}
--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -1,14 +1,27 @@
+import { Button } from './'
+import data from '../data.json'
+
 export const Introduction = () => (
   <div className='bg-white py-24 sm:py-32'>
     <div className='mx-auto max-w-7xl px-6 lg:px-8'>
       <div className='mx-auto max-w-2xl lg:mx-0'>
-        <h2 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+        <h2 className='text-5xl font-bold tracking-tight text-gray-900 sm:text-7xl'>
           Home of Nav
         </h2>
-        <p className='mt-6 text-lg leading-8 text-gray-600'>
+        <p className='mt-6 text-3xl leading-10 text-gray-600'>
           A perfectly cromulent product engineer, aspiring indiehacker, chronic
           oversharer of all things mental health
         </p>
+        {/* inline flex list of buttons */}
+        <ul className='mt-8 flex flex-row space-x-2'>
+          {data.links.map((link, index) => (
+            <li key={index}>
+              <Button href={link.url} variant='primary' size='lg'>
+                {link.name}
+              </Button>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   </div>
