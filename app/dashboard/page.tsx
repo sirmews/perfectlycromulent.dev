19dbc677e05f496a77df8140b7a62aa53461db0d
Navishkar Rao <nav@navrao.me>
2023-06-15 19:39:58 +1000

    adding thunder extension

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { Introduction } from '@components/Introduction'
 
 const HomeData = {
---
+++ app/resume/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <div>
+        <p>Mew</p>
+      </div>
+    </main>
+  )
+}
