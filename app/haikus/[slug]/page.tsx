aacf96c8261416178826c17e0e8db15220ae9af3
Nav Rao <sirmews@gmail.com>
2023-07-09 11:16:52 +0000

    adding old haikus

---
+++ app/haikus/[slug]/page.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import haikus from '@data/haikus.json'
+import { Haiku, Hero } from '@components/index'
+import { notFound } from 'next/navigation'
+
+interface Haiku {
+  id: string
+  haiku: string
+  date: string
+}
+
+export interface HaikuProps {
+  haiku: Haiku
+}
+
+interface PageProps {
+  params: {
+    slug: string
+  }
+}
+
+/**
+ * Split string into individual lines of text
+ * @param haiku
+ * @returns
+ */
+const splitHaiku = (haiku: string) => {
+  // split haiku into lines
+  const lines = haiku.split('/')
+  return lines
+}
+
+export default function Page({ params }: PageProps) {
+  // get haiku from router query
+  const { slug } = params
+  const haiku = haikus.find((haiku) => haiku.id === slug)
+
+  // return 404 if no haiku found
+  if (!haiku) {
+    notFound()
+  }
+
+  const textLines = splitHaiku(haiku.haiku)
+
+  return (
+    <div className='flex h-screen'>
+      <div className='m-auto'>
+        <Hero>
+          {textLines.map((line, index) => (
+            <>
+              {line}
+              <br />
+            </>
+          ))}
+        </Hero>
+        {haiku?.date && (
+          <p className='text-gray-500 text-sm py-2'>{haiku.date}</p>
+        )}
+      </div>
+    </div>
+  )
+}
---
+++ app/haikus/page.tsx
@@ -0,0 +1,21 @@
+import { Cursor, Hero, Haiku } from '@components/index'
+import haikus from '@data/haikus.json'
+
+export default function Page() {
+  return (
+    <main className='flex h-screen'>
+      <div className='space-y-12 pt-24'>
+        <Hero>
+          Perfectly Cromulent Haikus.
+          <br />
+          When inspiration strikes in 5-7-5 syllables.
+        </Hero>
+        <div className='space-y-20 pt-20'>
+          {haikus.reverse().map((haiku) => (
+            <Haiku key={haiku.id} haiku={haiku} />
+          ))}
+        </div>
+      </div>
+    </main>
+  )
+}
