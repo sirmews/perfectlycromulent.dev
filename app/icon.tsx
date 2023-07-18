432807737ba0a2bf0352919616d8b4fce6276db4
Nav Rao <sirmews@gmail.com>
2023-07-18 11:00:19 +0000

    non vercel icon

---
+++ app/icon.tsx
@@ -0,0 +1,37 @@
+import { ImageResponse } from 'next/server'
+
+export function generateImageMetadata() {
+  return [
+    {
+      contentType: 'image/png',
+      size: { width: 48, height: 48 },
+      id: 'small',
+    },
+    {
+      contentType: 'image/png',
+      size: { width: 72, height: 72 },
+      id: 'medium',
+    },
+  ]
+}
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: 300,
+          background: '#171717',
+          color: '#ffffff',
+        }}
+      >
+        Nav
+      </div>
+    )
+  )
+}
