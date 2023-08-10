8858e3c9e1e3f1dd9fe12b08de0c0ec4ba50fa13
Nav Rao <sirmews@gmail.com>
2023-08-10 11:23:55 +0000

    will this fix the ordering issue?

--- app/haikus/page.tsx
+++ app/haikus/page.tsx
@@ -1,12 +1,13 @@
-import { Haiku, Hero } from '@components/index'
-import haikus from '@data/haikus.json'
-import { Metadata } from 'next'
+import { Haiku, Hero } from '@components/index';
+import haikus from '@data/haikus.json';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   description: `Perfectly Cromulent Haikus - it's just 5-7-5 syllables`,
 }
 
-export default function Page() {
+export default async function Page() {
+  const haikus = await getData();
   return (
     <main className='flex min-h-screen'>
       <div className='space-y-12 pt-24 pb-24'>
@@ -16,11 +17,18 @@ export default function Page() {
           When inspiration strikes in 5-7-5 syllables.
         </Hero>
         <div className='space-y-20 pt-20'>
-          {haikus.reverse().map((haiku) => (
+          {haikus.map((haiku) => (
             <Haiku key={haiku.id} haiku={haiku} className='block' />
           ))}
         </div>
       </div>
     </main>
   )
+}
+
+
+// use backend to retrieve the haikus
+const getData = async () => {
+  const reversedHaikus = haikus.reverse();
+  return reversedHaikus;
 }
