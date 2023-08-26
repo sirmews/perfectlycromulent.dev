5632041122bab377071de84f2458de6d9c86d864
Nav Rao <nav.rao@mindsethealth.com>
2023-08-26 14:21:16 +0000

    more novu api implementations

--- app/api/notifications/subscribe/route.ts
+++ app/api/notifications/subscribe/route.ts
@@ -1,16 +1,12 @@
-import { createSubscriber, deleteSubscriber } from '@utils/novu';
-import { NextRequest } from 'next/server';
+import { createSubscriber } from '@utils/novu';
+import type { NextRequest } from 'next/server';
 
 type PostData = {
 	email: string;
 	description: string;
 }
 
-type DeleteData = {
-	subscriberId: string;
-}
-
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest): Promise<Response> {
 	try {
 		const { email, description } = await req.json() as PostData;
 		// TODO: better validation when its needed
@@ -21,15 +17,5 @@ export async function POST(req: NextRequest) {
 		return new Response('Successfully subscribed', { status: 200 });
 	} catch (error) {
 		return new Response(`Subscription error`, { status: 500 });
-	}
-}
-
-export async function DELETE(req: Request) {
-	try {
-		const { subscriberId } = await req.json() as DeleteData;
-		await deleteSubscriber(subscriberId);
-		return new Response('Successfully deleted subscriber', { status: 200 });
-	} catch (error) {
-		return new Response(`Deletion error`, { status: 500 });
 	}
 }
