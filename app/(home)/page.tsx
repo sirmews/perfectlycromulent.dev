85b0c3a6acdf522166ddefc9bf54f843a966ece9
Navishkar Rao <sirmews@gmail.com>
2024-07-20 11:35:03 +1000

    fixes

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from 'next'
 import Link from 'next/link'
 import { Cursor, Hero, StyledLink, SubHero } from '../components/index'
-import { useState } from "react";
 import Random from './Random'
 
 const heading: string = `Nav is a perfectly cromulent product engineer, an aspiring indiehacker
@@ -13,16 +12,6 @@ export const metadata: Metadata = {
 
 
 export default function Home() {
-
-  const [position, setPosition] = useState({ top: 50, left: 50 });
-
-  const handleMouseOver = () => {
-    const maxX = window.innerWidth - 100; // Assuming the button's width is 100px
-    const maxY = window.innerHeight - 50; // Assuming the button's height is 50px
-    const randomX = Math.random() * maxX;
-    const randomY = Math.random() * maxY;
-    setPosition({ top: randomY, left: randomX });
-  };
   
   return (
     <main className='flex min-h-screen flex-col'>
