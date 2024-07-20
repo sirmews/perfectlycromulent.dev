45708b044d85e08dd64da04a1066c3272eea16fb
Navishkar Rao <sirmews@gmail.com>
2024-07-20 11:28:34 +1000

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -11,6 +11,17 @@ export const metadata: Metadata = {
 
 
 export default function Home() {
+
+  const [position, setPosition] = useState({ top: 50, left: 50 });
+
+  const handleMouseOver = () => {
+    const maxX = window.innerWidth - 100; // Assuming the button's width is 100px
+    const maxY = window.innerHeight - 50; // Assuming the button's height is 50px
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+    setPosition({ top: randomY, left: randomX });
+  };
+  
   return (
     <main className='flex min-h-screen flex-col'>
       <div className='space-y-12'>
@@ -53,10 +64,10 @@ export default function Home() {
           </ul>
         </SubHero>
         <SubHero>
-          Contact me
           <ul className='space-y-4 mt-2'>
             <li>
-              <Link className='border-b-4 cursor-pointer hover:bg-simpsons-yellow hover:text-gray-900' href='mailto:woofmew@protonmail.com' id="hover">Click here</Link>
+              <Link className='border-b-4 cursor-pointer hover:bg-simpsons-yellow hover:text-gray-900' href='mailto:woofmew@protonmail.com' style={{ position: 'absolute', top: `${position.top}px`, left: `${position.left}px`, transition: 'top 0.3s, left 0.3s' }}
+        onMouseOver={handleMouseOver}>Click here</Link>
             </li>
           </ul>
         </SubHero>
